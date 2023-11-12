9242a37ef7804ae97c77c68daafdf631638aee01
Mike Wong <mikewongtkd@gmail.com>
2023-11-12 08:33:30 +0000

    Adding QR code detecting ability

--- frontend/things/picture.php
+++ frontend/things/picture.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . "/../vendor/autoload.php";
+use Zxing\QrReader;
 include_once( '../lib/uuid.php' );
 
 if( ! isset( $_POST[ 'png' ])) {
@@ -14,9 +16,21 @@ function save_png_and_exit( $png ) {
   $uuid = isset( $_POST[ 'uuid' ]) ? $_POST[ 'uuid' ] : null;
   if( is_null( $uuid )) { $uuid = MyshtUUID::new(); }
   error_log( "UUID: $uuid\n" ); # MW
-  $file = "images/$uuid.png";
-  $png  = preg_replace( '/^data\:image\/png;base64,/', '', $png );
-  $data = base64_decode( $png );
+  $file   = "images/$uuid.png";
+  $png    = preg_replace( '/^data\:image\/png;base64,/', '', $png );
+  $data   = base64_decode( $png );
+  $qrcode = new QrReader( $data, QrReader::SOURCE_TYPE_BLOB );
+
+  # ===== IF QR CODE CONTAINS VALID UUID, RETURN UUID AND EXIT
+  if( $qrcode->getResult()) {
+    $uuid = $qrcode->text();
+    if( MyshtUUID::validate( $uuid )) {
+      echo( "{\"uuid\":\"$uuid\"}\n" );
+      exit();
+    }
+  }
+
+  # ===== OTHERWISE STORE THE PICTURE, UPDATE THE DATABASE, AND RETURN THE ASSIGNED UUID
   $fp = fopen( $file, 'wb' );
   if( $fp ) {
     fwrite( $fp, $data );
