94772754a3f34407802603c3fb7a0dfcc6d46b47
Mike Wong <mikewongtkd@gmail.com>
2023-11-25 16:00:08 -0800

    Adding advanced search features

--- backend/db-init.sql
+++ backend/db-init.sql
@@ -4,7 +4,21 @@ create table document (
 	uuid text primary key,
 	class text not null,
 	data text_json default '{}',
-	deleted boolean default false
+	deleted text default null,
+	created text default current_timestamp,
+	modified text default current_timestamp,
+	seen text default current_timestamp
+);
+
+create table document_group (
+	a text not null,
+	b text not null,
+	description text not null,
+	deleted text default null,
+	created text default current_timestamp,
+	modified text default current_timestamp,
+	seen text default current_timestamp
+	primary key (a, b)
 );
 
 drop index if exists document_class;
--- frontend/index.php
+++ frontend/index.php
@@ -17,14 +17,24 @@ $db = new SQLite3( '/usr/local/mysht/db.sqlite' );
     <!-- Bootstrap core CSS -->
     <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- FontAwesome CSS -->
+    <link href="vendor/fontawesome-free-6.4.2-web/css/all.min.css" rel="stylesheet">
+
     <!-- Custom styles for this template -->
     <link href="assets/css/album.css" rel="stylesheet">
 
     <!-- CSS for Photo capture -->
     <link href="update/ssl.css" rel="stylesheet">
 
+    <style>
+      #advanced-search { margin-top: 2em; text-align: left; }
+      .btn-class {
+        width: calc( 50% - 22.5px );
+        margin-left: 20px;
+        margin-right: 0;
+      }
+    </style>
   </head>
-
   <body>
 
 <?php include_once( 'components/header.php' ); ?>
@@ -39,8 +49,58 @@ $db = new SQLite3( '/usr/local/mysht/db.sqlite' );
           <p>
             <a href="update.php" class="btn btn-primary my-2"  >Add Things</a>
             <a href="return.php" class="btn btn-secondary my-2">Return Things</a>
-            <form method="post" action="search.php">
-              <input type="search" class="form-control ds-input" id="search-input" placeholder="Search..." aria-label="Search for..." autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-owns="algolia-autocomplete-listbox-0" dir="auto" style="position: relative; vertical-align: top;">
+            <form>
+              <div class="row">
+                <div class="col-8">
+                  <input type="search" class="form-control ds-input" name="search" id="search" placeholder="Search..." aria-label="Search for..." autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-owns="algolia-autocomplete-listbox-0">
+                </div>
+                <div class="col-1">
+                  <button class="btn btn-success" id="btn-search"><span class="fa-solid fa-magnifying-glass"></span></button>
+                </div>
+                <div class="col-3 d-grid">
+                  <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#advanced-search" id="btn-advanced-search">Advanced</button>
+                </div>
+              </div>
+              <div class="collapse" id="advanced-search">
+                <div class="card card-body">
+                  <div class="mb-3">
+                    <div class="row">
+                      <div class="col-2">
+                        <label for="classes">Classes</label>
+                      </div>
+                      <div class="col-10">
+                        <div class="classes">
+                          <button class="btn btn-toggle btn-class btn-primary active" name="class-thing" id="btn-class-thing">Things</button>
+                          <button class="btn btn-toggle btn-class btn-primary active" name="class-place" id="btn-class-place">Places</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="row">
+                      <div class="col-4">
+                        <label for="date">Search over Date Range</label>
+                        <select class="form-control" name="date">
+                          <option value="none">Select one...</option>
+                          <option value="created">Created</option>
+                          <option value="modified">Modified</option>
+                          <option value="seen">Last Seen</option>
+                        </select>
+                      </div>
+                      <div class="col-4">
+                        <label for="start-date">Start Date</label>
+                        <input class="form-control" type="date" name="start-date"></input>
+                      </div>
+                      <div class="col-4">
+                        <label for="end-date">End Date</label>
+                        <input class="form-control" type="date" name="end-date"></input>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                  </div>
+                </div>
+              </div>
             </form>
           </p>
         </div>
@@ -96,8 +156,46 @@ foreach( $rows as $row ):
       </div>
     </footer>
 
-    <script src="vendor/axllent/jquery/jquery.slim.min.js"></script>
+    <script src="vendor/axllent/jquery/jquery.min.js"></script>
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+      $( '.btn-toggle' ).click( ev => {
+        ev.preventDefault();
+        let target = $( ev.target );
+        if( target.parent().hasClass( 'btn-toggle' )) { target = target.parent(); }
+
+        if( target.hasClass( 'active' )) {
+          target.addClass( 'btn-secondary' );
+          target.removeClass( 'btn-primary' );
+          target.removeClass( 'active' );
+
+        } else {
+          target.addClass( 'btn-primary' );
+          target.addClass( 'active' );
+          target.removeClass( 'btn-secondary' );
+        }
+      });
+      $( '#btn-advanced-search' ).click( ev => { ev.preventDefault(); });
+      $( '#btn-search' ).click( ev => { 
+        ev.preventDefault(); 
+        let message = {};
+        $( 'form .form-control' ).each(( i, el ) => {
+          let input = $( el );
+          message[ input.attr( 'name' )] = input.val();
+        });
+
+        $( 'form .btn-toggle' ).each(( i, el ) => {
+          let input = $( el );
+          message[ input.attr( 'name' )] = input.hasClass( 'active' );
+        });
+
+        console.log( message ); // MW
+        $.post( 'db.php', message )
+        .then( response => {
+          console.log( response ); // MW
+        });
+      });
+    </script>
   </body>
 </html>
 <!-- vim: set ts=2 sw=2 expandtab nowrap: -->
